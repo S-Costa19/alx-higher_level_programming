3286c76b407b76c4a7d5e5594e3ddcad7b75f217
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-09-12 14:26:17 -0700

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
+
+
 def read_file(filename=""):
-    try:
-        with open(filename, mode='r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
-
-    except FileNotFoundError:
-        pass  # File not found, do nothing
-
-if __name__ == "__main__":
-
-
-    read_file("my_file_0.txt")
-
-# Add two blank lines here to satisfy the formatting requirement
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")

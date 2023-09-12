95fb2599c635e6f3512232ca79fa4e22ed975822
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-09-12 13:26:46 -0700

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
-
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    try:
+        with open(filename, mode='r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')  # Print each line without adding extra newline
+
+    except FileNotFoundError:
+        pass  # File not found, do nothing
+
+# Example usage:
+if __name__ == "__main__":
+    read_file("example.txt")  # Replace with your file name
+

ee086921e67fa70b942586275db23f94dd0b6ae5
S-Costa19 <shamsuddeenmuazu11@gmail.com>
2023-09-12 14:06:36 -0700

    done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-
 def read_file(filename=""):
     try:
         with open(filename, mode='r', encoding='utf-8') as file:
             for line in file:
-                print(line, end='')  # Print each line without adding extra newline
+                print(line, end='')
 
     except FileNotFoundError:
         pass  # File not found, do nothing
 
-# Example usage:
 if __name__ == "__main__":
-    read_file("example.txt")  # Replace with your file name
 
+
+    read_file("my_file_0.txt")
+
+# Add two blank lines here to satisfy the formatting requirement
